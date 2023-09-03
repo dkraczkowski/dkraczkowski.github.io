028e4a494098aec03c683690aaab4d470d60875c
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2023-09-03 08:31:15 +0200

    Move ci article to submodule

--- articles/github_actions_python_aws/src/my_project/handler.py
+++
@@ -1,5 +0,0 @@
-from typing import Any
-
-
-def handle(event: Any, context: Any) -> None:
-    print("Hello, World!")
--- articles/github_actions_python_aws/tests/test_example.py
+++
@@ -1,5 +0,0 @@
-from my_project.handler import handle
-
-
-def test_example():
-    handle(None, None)
